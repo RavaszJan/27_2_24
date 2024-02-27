27de5ed5d8adf8ed4f125c7d7c01911b292781a7
RavaszJan <janravasz@gmail.com>
2024-02-27 19:27:04 +0100

    test1

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 
-# 27.2.24
+# 27.2.24 Testing
+
+import  unittest
+from main import Kalkulacka
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_integers(self):
+        calc=Kalkulacka()
+        self.assertEqual(calc.sucet(1,2),3)
+
+    def test_add_negative(self):
+        calc=Kalkulacka()
+        self.assertAlmostEqual(calc.sucet(-1,-1),-2)
+
+    def test_add_floats(self):
+        calc=Kalkulacka()
+        self.assertAlmostEqual(calc.sucet(1.1,2.2,3.3, places=1))
+
+    if __name__ == "__main__":
+        unittest.main()
+
